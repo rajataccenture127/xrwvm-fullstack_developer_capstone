3228fc041241fa23d35710f8a560f8e1cba7bf5d
rajataccenture127 <rajat.accenture127@gmail.com>
2026-02-02 05:55:01 -0500

    Fix flake8 issues in restapis

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,18 +3,25 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    "backend_url",
+    default="http://localhost:3030",
+)
+
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url",
+    default="http://localhost:5050/",
+)
+
 
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
+
+    if kwargs:
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
 
@@ -27,9 +34,11 @@ def get_request(endpoint, **kwargs):
     except Exception as err:
         print(f"Error: {err}")
 
+
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
+
     try:
         response = requests.get(request_url)
         return response.json()
@@ -41,6 +50,7 @@ def analyze_review_sentiments(text):
 # Add code for posting review
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
+
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
