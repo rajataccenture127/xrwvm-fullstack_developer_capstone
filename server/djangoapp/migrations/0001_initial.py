06c7442250e86f3c433bb5b6fa733f57ed9040fc
rajataccenture127 <rajat.accenture127@gmail.com>
2026-02-02 05:46:59 -0500

    Fix flake8 issues in migration file

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -10,39 +10,82 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='CarMake',
+            name="CarMake",
             fields=[
-                ('id', models.BigAutoField(auto_created=True,
-                primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('description', models.TextField(blank=True)),
-                ('created_at', models.DateTimeField(
-                    default=django.utils.timezone.now)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("description", models.TextField(blank=True)),
+                (
+                    "created_at",
+                    models.DateTimeField(
+                        default=django.utils.timezone.now,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='CarModel',
+            name="CarModel",
             fields=[
-                ('id', models.BigAutoField(auto_created=True,
-                primary_key=True, serialize=False, verbose_name='ID')),
-                ('dealer_id', models.IntegerField()),
-                ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), 
-                ('SUV', 'SUV'), ('WAGON', 'Wagon'), ('SPORT', 'Sport'), 
-                ('HATCHBACK', 'Hatchback')], default='SUV', max_length=20)),
-                ('year', models.IntegerField(default=2023, 
-                validators=[django.core.validators.MinValueValidator(2015), 
-                django.core.validators.MaxValueValidator(2023)])),
-                ('created_at', 
-                models.DateTimeField(default=django.utils.timezone.now)),
-                ('car_make', 
-                models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, 
-                to='djangoapp.carmake')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("dealer_id", models.IntegerField()),
+                ("name", models.CharField(max_length=100)),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[
+                            ("SEDAN", "Sedan"),
+                            ("SUV", "SUV"),
+                            ("WAGON", "Wagon"),
+                            ("SPORT", "Sport"),
+                            ("HATCHBACK", "Hatchback"),
+                        ],
+                        default="SUV",
+                        max_length=20,
+                    ),
+                ),
+                (
+                    "year",
+                    models.IntegerField(
+                        default=2023,
+                        validators=[
+                            django.core.validators.MinValueValidator(2015),
+                            django.core.validators.MaxValueValidator(2023),
+                        ],
+                    ),
+                ),
+                (
+                    "created_at",
+                    models.DateTimeField(
+                        default=django.utils.timezone.now,
+                    ),
+                ),
+                (
+                    "car_make",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="djangoapp.carmake",
+                    ),
+                ),
             ],
         ),
     ]
